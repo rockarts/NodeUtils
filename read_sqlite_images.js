92f43a94b9c8bd039dd7045cb33d40632b97004a
Steven Rockarts <stevenrockarts@gmail.com>
2017-09-22 16:47:44 -0600

    Update read_sqlite_images.js

--- read_sqlite_images.js
+++ read_sqlite_images.js
@@ -1,3 +1,5 @@
+const readChunk = require('read-chunk'); // npm install read-chunk 
+const imageType = require('image-type');
 var fs = require("fs");
 var file = "test.sqlite";
 var exists = fs.existsSync(file);
@@ -8,11 +10,22 @@ var db = new sqlite3.Database(file, sqlite3.OPEN_READONLY);
 db.serialize(function() {
     db.each("SELECT id, picture FROM route where picture is not null", function(err, row) {
         console.log(row.id + ".jpg");
+        
+        
         fs.writeFile(row.id + ".jpg", row.picture, function(err) {
             if(err) {
                 return console.log(err);
             }
         });
+
+        fs.writeFile(row.id + ".png", row.picture, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+        });
+
+        const buffer = readChunk.sync('18.png', 0, 12);
+        console.log(imageType(buffer));
     });
 });
 
