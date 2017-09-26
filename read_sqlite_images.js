0bc6cf16ced0db6ffefb6d9157d7756d8db8f639
Steven Rockarts <stevenrockarts@gmail.com>
2017-09-26 13:35:34 -0600

    Update read_sqlite_images.js
    Write out the proper file type with extension.

--- read_sqlite_images.js
+++ read_sqlite_images.js
@@ -9,23 +9,34 @@ var db = new sqlite3.Database(file, sqlite3.OPEN_READONLY);
 
 db.serialize(function() {
     db.each("SELECT id, picture FROM route where picture is not null", function(err, row) {
-        console.log(row.id + ".jpg");
-        
-        
-        fs.writeFile(row.id + ".jpg", row.picture, function(err) {
+       //console.log(row.id + ".jpg");
+       
+
+        fs.writeFile(row.id + ".img", row.picture, function(err) {
             if(err) {
                 return console.log(err);
+            }else{
+                const buffer = readChunk.sync(row.id + '.img', 0, 12);
+                console.log(row.id + '.img');
+                console.log(imageType(buffer));
+                if(imageType(buffer).ext == 'jpg') {
+                    console.log("JPEG!");
+                    fs.rename(row.id + ".img", row.id + ".jpg", function(err) {
+                        if(err) {
+                            return console.log(err);
+                        }
+                    });
+                }
+                if(imageType(buffer).ext == 'png') {
+                    console.log("PNG!");
+                    fs.rename(row.id + ".img", row.id + ".png", function(err) {
+                        if(err) {
+                            return console.log(err);
+                        }
+                    });
+                }
             }
         });
-
-        fs.writeFile(row.id + ".png", row.picture, function(err) {
-            if(err) {
-                return console.log(err);
-            }
-        });
-
-        const buffer = readChunk.sync('18.png', 0, 12);
-        console.log(imageType(buffer));
     });
 });
 
